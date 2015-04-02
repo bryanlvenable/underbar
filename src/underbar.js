cb01e9f67e6769d0e8f3b01ce579eb32a694bb0f
Bryan L. Venable <bryanlvenable@gmail.com>
2015-04-01 17:31:28 -0700

    completed reduce

--- src/underbar.js
+++ src/underbar.js
@@ -152,6 +152,13 @@ var _ = {};
   //     return total + number;
   //   }, 0); // should be 6
   _.reduce = function(collection, iterator, accumulator) {
+    if(accumulator === undefined){
+      accumulator = collection[0];
+    }
+    _.each(collection, function(value) {
+      accumulator = iterator(accumulator, value);
+    });
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
@@ -170,6 +177,9 @@ var _ = {};
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    return _.reduce(collection, function(total, value) {
+
+    })
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
